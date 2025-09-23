59d549494b8063af7acef5e32b60e45fb1c155fb
davidx345 <davidayo2603@gmail.com>
2025-09-23 01:17:14 +0100

    ffff

--- frontend/src/contexts/NotificationContext.tsx
+++ frontend/src/contexts/NotificationContext.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { createContext, useContext, useEffect, useState } from 'react'
-import { getFCMToken, onForegroundMessage } from '@/lib/firebase'
+import { getFCMToken, onForegroundMessage } from '../lib/firebase'
 import { useAuth } from './AuthContext'
 import { useToast } from './ToastContext'
 
